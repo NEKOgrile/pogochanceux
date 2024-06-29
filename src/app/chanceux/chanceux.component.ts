afe4d33eab56a41eac408f3ca50ccc9a2fe54bea
willem <willemcornil@gmail.com>
2024-06-29 16:55:50 +0200

    cart color

--- src/app/chanceux/chanceux.component.ts
+++ src/app/chanceux/chanceux.component.ts
@@ -4,8 +4,9 @@ import { HttpClient } from '@angular/common/http';
 interface Pokemon {
   id: string;
   name: string;
-  type: string; // Vous pouvez définir le type correct ici
-  imageUrl: string; // URL de l'image du Pokémon
+  primaryType: string;
+  secondaryType: string | null;
+  imageUrl: string;
 }
 
 @Component({
@@ -14,57 +15,29 @@ interface Pokemon {
   styleUrls: ['./chanceux.component.scss']
 })
 export class ChanceuxComponent implements OnInit {
-  colClass: string = 'col-md-4'; // Par défaut, 3 cartes par ligne
-  displayMode: string = 'default'; // Mode d'affichage par défaut
+  colClass: string = 'col-md-4';
+  displayMode: string = 'default';
   pokemons: Pokemon[] = [];
 
   constructor(private http: HttpClient) {}
 
-  async ngOnInit() {
-    await this.loadPokemonData();
+  ngOnInit(): void {
+    this.loadPokemonData();
   }
 
-  async loadPokemonData() {
-    try {
-      const data: any = await this.http.get('https://raw.githubusercontent.com/pokealarm/pokealarm/dev/locales/fr.json').toPromise();
-      const pokemonData = data['pokemon'];
-      const pokemonsArray: Pokemon[] = Object.keys(pokemonData).map(id => ({
-        id: id,
-        name: pokemonData[id],
-        type: 'Unknown', // Vous pouvez définir le type plus tard si nécessaire
-        imageUrl: this.formatImageUrl(parseInt(id)) // Formater l'URL de l'image
-      }));
+  loadPokemonData() {
+    this.http.get<any[]>('https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json')
+      .subscribe((data: any[]) => {
+        this.pokemons = data.map(pokemon => ({
+          id: pokemon.dexNr.toString().padStart(3, '0'),
+          name: pokemon.names.French,
+          primaryType: pokemon.primaryType.names.French,
+          secondaryType: pokemon.secondaryType ? pokemon.secondaryType.names.French : null,
+          imageUrl: this.formatImageUrl(pokemon.dexNr.toString())
+        }));
 
-      // Trier pokemonsArray par ID en utilisant une fonction de comparaison numérique
-      pokemonsArray.sort((a, b) => this.compareIds(a.id, b.id));
-
-      console.log('Pokemons array:', pokemonsArray.map(p => p.id));
-      this.pokemons = pokemonsArray;
-    } catch (error) {
-      console.error('Erreur lors du chargement des données Pokémon:', error);
-      // Gestion des erreurs, par exemple afficher un message à l'utilisateur
-    }
-  }
-
-  // Fonction pour comparer les IDs (tri numérique)
-  private compareIds(idA: string, idB: string): number {
-    const numA = parseInt(idA, 10);
-    const numB = parseInt(idB, 10);
-
-    // Si les deux IDs sont des nombres, les comparer numériquement
-    if (!isNaN(numA) && !isNaN(numB)) {
-      return numA - numB;
-    }
-
-    // Sinon, comparer les IDs comme des chaînes
-    return idA.localeCompare(idB);
-  }
-
-  // Fonction pour formater l'URL de l'image du Pokémon
-  private formatImageUrl(id: number): string {
-    const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-    // Ne pas ajouter de zéro devant le numéro de l'ID
-    return `${baseUrl}${id}.png`;
+        console.log('Pokemons chargés :', this.pokemons);
+      });
   }
 
   setColumns(count: number) {
@@ -73,7 +46,14 @@ export class ChanceuxComponent implements OnInit {
   }
 
   onPokemonClicked(event: any) {
-    console.log(`Pokemon ID ${event.id} de type ${event.type} cliqué`);
-    // Logique pour ajouter à la base de données ou autre traitement
+    console.log(`Pokemon ID ${event.id} de type ${event.primaryType} cliqué`);
   }
+
+    // Fonction pour formater l'URL de l'image du Pokémon
+    private formatImageUrl(id: number): string {
+      const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+      // Ne pas ajouter de zéro devant le numéro de l'ID
+      return `${baseUrl}${id}.png`;
+    }
+
 }
--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -7,11 +7,38 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class PokemonCardComponent {
   @Input() pokemon: any;
-  @Input() type!: string; // Utilisation de l'opérateur de non-nullité pour indiquer que cette propriété sera initialisée plus tard.
   @Output() pokemonClicked = new EventEmitter<any>();
-  @Input() displayMode: string = 'default'; // Ajoutez cette ligne
+  @Input() displayMode: string = 'default';
+
+  constructor() {}
 
   onCardClick() {
-    this.pokemonClicked.emit({ id: this.pokemon.id, type: this.type });
+    this.pokemonClicked.emit({ id: this.pokemon.id });
+  }
+
+  getHeaderClass(): string {
+    if (this.pokemon.secondaryType != null) {
+      return `${this.pokemon.primaryType.toLowerCase()}-header-${this.pokemon.secondaryType.toLowerCase()}-header`;
+    } else if (this.pokemon.secondaryType == null) {
+      return `${this.pokemon.primaryType.toLowerCase()}-header`;
+    } else {
+      return '';
+    }
+  }
+
+  getCardBodyStyles() {
+    let styles = {};
+
+    if (this.pokemon.primaryType && this.pokemon.secondaryType) {
+      styles = {
+        'background': `linear-gradient(to bottom right, var(--${this.pokemon.primaryType}), var(--${this.pokemon.secondaryType}))`
+      };
+    } else if (this.pokemon.primaryType) {
+      styles = {
+        'background-color': `var(--${this.pokemon.primaryType})`
+      };
+    }
+
+    return styles;
   }
 }
