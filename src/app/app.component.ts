97d8010f0f0c06e6c9f896c6fb9e65c140b2ed5e
willem <willemcornil@gmail.com>
2024-06-29 22:16:58 +0200

    nav bar et defilement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth.service'; // Assurez-vous d'importer AuthService correctement
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service'; // Assurez-vous d'importer correctement votre service d'authentification
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  username: string | null = null;
+
   constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    // S'abonner aux changements d'état d'authentification
+    this.authService.isLoggedIn$().subscribe((loggedIn: boolean) => {
+      this.isLoggedIn = loggedIn;
+      this.username = this.authService.getUsername();
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   private apiUrl = 'http://localhost:3000/api';
 
-  constructor(private http: HttpClient) { }
+  // BehaviorSubject pour l'état d'authentification
+  private isLoggedInSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {
+    // Initialisation de l'état d'authentification au chargement du service
+    this.isLoggedInSubject.next(this.isLoggedIn());
+  }
+
+  // Méthode pour s'abonner aux changements d'état d'authentification
+  isLoggedIn$(): Observable<boolean> {
+    return this.isLoggedInSubject.asObservable();
+  }
 
   register(user: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/register`, user);
@@ -21,6 +31,7 @@ export class AuthService {
       tap((response: any) => {
         if (typeof localStorage !== 'undefined') {
           localStorage.setItem('username', username);
+          this.isLoggedInSubject.next(true); // Mettre à jour l'état d'authentification
         }
       })
     );
@@ -29,6 +40,7 @@ export class AuthService {
   logout(): void {
     if (typeof localStorage !== 'undefined') {
       localStorage.removeItem('username');
+      this.isLoggedInSubject.next(false); // Mettre à jour l'état d'authentification
     }
   }
 
